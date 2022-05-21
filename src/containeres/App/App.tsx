d132cac6c028d4577fcf6290748bcfc7b31a05eb
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-05-21 20:19:05 +0300

    feat: add base routes

--- src/containeres/App/App.tsx
+++ src/containeres/App/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 import './App.css';
+import HomePage from '../HomePage';
+import NotFoundPage from '../NotFoundPage';
+import SignInPage from '../SignInPage';
+import MainPage from '../MainPage';
+import BoardPage from '../BoardPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className='wrapper'>
+      <Header />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="signIn" element={<SignInPage />} /> 
+        <Route path="main" element={<MainPage />} /> 
+        <Route path="board" element={<BoardPage />} /> 
+        <Route path="*" element={<NotFoundPage />} />        
+      </Routes>
+      <Footer />
     </div>
+  </BrowserRouter>
   );
 }
 
