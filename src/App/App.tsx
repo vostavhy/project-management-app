4ceaac63f0d6757b3ff04449d53b288b42744520
Nickolay Malysh <stalcrot@yandex.ru>
2022-05-22 16:29:09 +0300

    feat: implement layout

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,20 +7,21 @@ import NotFoundPage from '../containers/NotFoundPage';
 import SignInPage from '../containers/SignInPage';
 import MainPage from '../containers/MainPage';
 import BoardPage from '../containers/BoardPage';
+import { Layout } from '../containers/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="wrapper">
-        <Header />
         <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="signIn" element={<SignInPage />} />
-          <Route path="main" element={<MainPage />} />
-          <Route path="board" element={<BoardPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="signIn" element={<SignInPage />} />
+            <Route path="main" element={<MainPage />} />
+            <Route path="board" element={<BoardPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
         </Routes>
-        <Footer />
       </div>
     </BrowserRouter>
   );
