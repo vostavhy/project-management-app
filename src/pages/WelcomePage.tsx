e05d92531c124fe3fd65ee2aae9ada81f58982fb
vostavhy <vostavhy@gmail.com>
2022-05-29 17:40:00 +0300

    feat: implement sticky footer

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,23 +1,21 @@
-import { Container, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import WelcomeButtons from '../components/WelcomeButtons';
 import WelcomeCreators from '../components/WelcomeCreators';
 import WelcomeDescription from '../components/WelcomeDescription';
 
 function WelcomePage() {
   return (
-    <Container sx={{ p: 7 }}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={8}>
-          <WelcomeDescription />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <WelcomeButtons />
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <WelcomeCreators />
-        </Grid>
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={8}>
+        <WelcomeDescription />
       </Grid>
-    </Container>
+      <Grid item xs={12} md={4}>
+        <WelcomeButtons />
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <WelcomeCreators />
+      </Grid>
+    </Grid>
   );
 }
 
