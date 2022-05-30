a40bc4dd59e1d0de6e99c7a05d8f6ab96bd7a0b0
vostavhy <vostavhy@gmail.com>
2022-05-31 01:57:51 +0300

    refactor: delete paddings

--- src/components/WelcomeDescription.tsx
+++ src/components/WelcomeDescription.tsx
@@ -4,7 +4,7 @@ export default function WelcomeDescription() {
   return (
     <Box>
       <Paper elevation={2}>
-        <Typography variant='h3' p={5} pb={2}>
+        <Typography variant='h3' p={5} py={2}>
           Simple app to manage your tasks & time.
         </Typography>
         <Typography component='p' variant='h5' p={3} pt={0}>
